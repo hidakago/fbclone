22f73c75c4dd263b02939f9d5ee0af9b66a0a76c
hidakago <hidakago@gmail.com>
2019-08-15 21:38:21 +0900

    usersコントローラの作成、picturesコントローラの作成、ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'sessions#new'
+  resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :pictures do
+    collection do
+      post :newconfirm
+    end
+    member do
+      put :editconfirm
+    end
+  end
 end
