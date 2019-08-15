1f4d730e1ef6a7e09224abf9a21333cf8c570344
hidakago <hidakago@gmail.com>
2019-08-15 18:08:30 +0900

    userモデルとpictureモデルにアソシエーション設定。userとpictureは1対多、pictureモデルにバリデーション追加

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,10 @@
 class Picture < ApplicationRecord
+  validates :comment_or_image, presence: true
   belongs_to :user
+
+  private
+
+  def comment_or_image
+    comment.presence or image.presence
+  end
 end
