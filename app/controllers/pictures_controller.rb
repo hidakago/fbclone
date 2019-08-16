032f8260dd1760825d957431d54a88c6f54dd8bf
hidakago <hidakago@gmail.com>
2019-08-16 18:06:23 +0900

    新規投稿画面、新規投稿確認画面の追加

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,2 +1,31 @@
 class PicturesController < ApplicationController
+  def new
+    @picture = Picture.new
+  end
+
+  def newconfirm
+    @picture = Picture.new(picture_params)
+    @picture.user_id = current_user.id
+    render :new if @picture.invalid?
+  end
+
+  def create
+    @picture = Picture.new(picture_params)
+    @picture.user_id = current_user.id
+    if params[:back]
+      render :new
+    else
+      if @picture.save
+        redirect_to user_path(current_user.id), notice: "投稿を作成しました！"
+      else
+        render :new
+      end
+    end
+  end
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:comment, :image, :user_id, :image_cache)
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+  def excerpt_comment(comment)
+    if comment.length > 100
+      comment = "#{comment.slice(0, 100)}・・・"
+    end
+    comment
+  end
 end
