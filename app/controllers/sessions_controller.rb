4f8f8c58503fd9186889b81432dd97d0514575f3
hidakago <hidakago@gmail.com>
2019-08-16 14:50:19 +0900

    ログイン機能の追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,20 @@
 class SessionsController < ApplicationController
   def new
-    redirect_to new_user_path
+    if logged_in?
+      redirect_to user_path(current_user.id)
+    else
+      redirect_to new_user_path
+    end
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)
+    else
+      flash[:danger] = 'ログインに失敗しました'
+      redirect_to new_user_path
+    end
   end
 end
